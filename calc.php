d7cde28cbbebc6c1dd4f3d2496a1cfb06d0f393a
Angel R <93774378+Angelgaeta@users.noreply.github.com>
2022-11-05 17:16:33 +0100

    Create calc.php

---
+++ calc.php
@@ -0,0 +1,20 @@
+<?php 
+
+echo <<<_END
+  <html>
+    <head>
+      <title>Utilisation de valeurs par défaut dans des champs de formulaire</title>
+    </head>
+    <body>
+      <form method="post" action="calc.php"><pre>
+        Capital emprunté <input type="text" name="capital">
+        Mensualité       <input type="text" name="mensualite">
+        Nombre d'années  <input type="text" name="annee" value="15">
+        Taux d'intérêt   <input type="text" name="taux" value="3">
+                         <input type="submit">
+      </pre></form>
+    </body>
+  </html>
+_END;
+
+?>
